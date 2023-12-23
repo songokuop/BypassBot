3350a018d40a2b51f0b0b37382d1cb90b82f39ab
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-23 15:22:17 +0100

    Update __init__.py

--- FZBypass/__init__.py
+++ FZBypass/__init__.py
@@ -27,6 +27,7 @@ class Config:
         exit(1)
     AUTO_BYPASS     = getenv('AUTO_BYPASS', 'False').lower() == 'true'
     AUTH_CHATS = ('-1002005718645')
+    OWNER_ID        = int(getenv('OWNER_ID', 0))        
     DIRECT_INDEX    = getenv('DIRECT_INDEX', '').rstrip('/')
     LARAVEL_SESSION = getenv('LARAVEL_SESSION', '')
     XSRF_TOKEN      = getenv('XSRF_TOKEN', '')
