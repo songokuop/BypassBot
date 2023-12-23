249aff90ead88b7d0a8c5fa20c318de15fa94bfe
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-23 15:57:30 +0100

    Update __init__.py

--- FZBypass/__init__.py
+++ FZBypass/__init__.py
@@ -26,8 +26,8 @@ class Config:
         LOGGER.critical('ENV Missing. Exiting Now...')
         exit(1)
     AUTO_BYPASS     = getenv('AUTO_BYPASS', 'False').lower() == 'true'
-    AUTH_CHATS = ('-1002008661072 1612')
-    OWNER_ID        = int(getenv('OWNER_ID', 0))        
+    AUTH_CHATS = ('-1002008661072')
+    OWNER_ID        = int(getenv('OWNER_ID', 1594245715))        
     DIRECT_INDEX    = getenv('DIRECT_INDEX', '').rstrip('/')
     LARAVEL_SESSION = getenv('LARAVEL_SESSION', '')
     XSRF_TOKEN      = getenv('XSRF_TOKEN', '')
